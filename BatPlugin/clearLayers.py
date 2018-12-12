e281d94f286f38ebf499ae14a7ae40d8e36b84c2
fernandobello85 <fernandobello.ocampo@gmail.com>
2018-12-12 20:40:00 +0100

    Clean repository

--- BatPlugin/clearLayers.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*
-from qgis.utils import iface
-from qgis.core import *
-from .compat2qgis import QgsProject
-
-def clearLayer(layer):
-        layers = QgsProject.instance().mapLayersByName(layer)
-        QgsProject.instance().removeMapLayers([layer.id() for layer in layers])
