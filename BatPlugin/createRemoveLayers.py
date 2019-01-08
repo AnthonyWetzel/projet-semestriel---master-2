5c8e46013ea3c39fde5b8e38d7b8ebea30a209b0
fernandobello85 <fernandobello.ocampo@gmail.com>
2019-01-08 14:45:48 +0100

    Code standarisation in CreateLineLayer and CreatePointLayer

--- BatPlugin/createRemoveLayers.py
+++ BatPlugin/createRemoveLayers.py
@@ -10,18 +10,17 @@ def createLayerLines(coordLines):
     clearLayer('lineLayer')
     # Specify the geometry type
     layer_line = QgsVectorLayer('LineString?crs=epsg:4230', 'lineLayer', 'memory')
-
     prov_line = layer_line.dataProvider()
-
+    # Create and add line features
     features = []
     for coordonnee in coordLines:
         x_res,y_res = dst(coordonnee[0], coordonnee[1], coordonnee[2], coordonnee[3])
         point = QgsPoint(coordonnee[1], coordonnee[0])
         point2 = QgsPoint(y_res,x_res)
         # Add a new feature and assign the geometry
-        feat = QgsFeature()
-        feat.setGeometry(QgsGeometry.fromPolyline([point, point2]))
-        features.append(feat)		
+        feat_line = QgsFeature()
+        feat_line.setGeometry(QgsGeometry.fromPolyline([point, point2]))
+        features.append(feat_line)		
     prov_line.addFeatures(features)
 
     # Update extent of the layer
@@ -29,21 +28,26 @@ def createLayerLines(coordLines):
     # Add the layer to the Layers panel
     QgsProject.instance().addMapLayers([layer_line])
 
-def createLayerPoints(coordPoint):
+def createLayerPoints(coordPoints):
     """Création d'un layer composé de points"""
     clearLayer('batLayer')
+    # Specify the geometry type
     layer_point = QgsVectorLayer('Point?crs=epsg:4230', 'batLayer', 'memory')
     prov_point = layer_point.dataProvider()
-
-    for point in coordPoint:
+    # Create and add point features
+    features = []
+    for point in coordPoints:
         inX = point[0]
         inY = point[1]
+        # Add a new feature and assign the geometry
         feat_point = QgsFeature()
-        geom_point = buildGeomPoint(inX, inY)
-        feat_point.setGeometry(geom_point)
-        prov_point.addFeatures([feat_point])
+        feat_point.setGeometry(buildGeomPoint(inX, inY))
+        features.append(feat_point)
+    prov_point.addFeatures(features)
 
+    # Update extent of the layer
     layer_point.updateExtents()
+    # Add the layer to the Layers panel
     QgsProject.instance().addMapLayers([layer_point])
 
 def clearLayer(layer):
